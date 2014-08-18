8a3f169c856d147770ca5fa8ee75934a606f9ea8
Moe <badakhms@gmail.com>
2014-08-18 01:17:44 -0400

    add missiing require bootstap in aplllication js

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,24 +25,25 @@ describe "Authentication" do
       before { click_link "Home" }
       it { should_not have_selector('div.alert.alert-error') }
     end
-  end
 
-  describe "with valid information" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { sign_in user }
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
 
-    it { should have_title(user.name) }
-    it { should have_link('Profile',     href: user_path(user)) }
-    it { should have_link('Users',       href: users_path) }
-    it { should have_link('Settings',    href: edit_user_path(user)) }
-    it { should have_link('Sign out',    href: signout_path) }
-    it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Users',       href: users_path) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
 
-    describe "followed by signout" do
-      before { click_link "Sign out" }
-      it { should have_link('Sign in') }
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
+
 
   describe "authorization" do
 
